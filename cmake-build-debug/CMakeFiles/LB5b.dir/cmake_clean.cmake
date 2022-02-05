be9ecb315782477738a657a94ccd1e6bedba8352
tttgtjft <1onecov@gmail.com>
2022-02-05 19:56:49 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/LB5b.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LB5b.dir/main.c.obj"
+  "LB5b.exe"
+  "LB5b.exe.manifest"
+  "LB5b.pdb"
+  "libLB5b.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LB5b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
